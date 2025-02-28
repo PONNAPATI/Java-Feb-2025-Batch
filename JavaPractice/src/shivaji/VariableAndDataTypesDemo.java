253b09129b10d918f88a1972079bc3b43f1789fd
shivajiparepally <pvshiva2101@gmail.com>
2025-02-27 21:15:38 -0500

    Added a sample class

---
+++ JavaPractice/src/shivaji/VariableAndDataTypesDemo.java
@@ -0,0 +1,19 @@
+package shivaji;
+
+public class VariableAndDataTypesDemo {
+    public static void main(String[] args) {
+        String name = "Shivaji";
+        int id = 1234567890;
+        double height = 5.10;
+        char gender = 'M';
+        boolean student = true;
+
+        System.out.println("Student name : "+name);
+        System.out.println("Studentr Id : "+id);
+        System.out.println("Height : "+height+"'" );
+        System.out.println("Gender "+gender);
+        System.out.println("Age > 18 :  "+student);
+
+
+    }
+}
