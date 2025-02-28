38d4ce7a3f7b8071137b90fd63ddba41edb05c7d
shivajiparepally <pvshiva2101@gmail.com>
2025-02-27 21:17:47 -0500

    Added a sample class

--- JavaPractice/src/shivaji/VariableAndDataTypesDemo.java
+++ JavaPractice/src/shivaji/VariableAndDataTypesDemo.java
@@ -9,7 +9,7 @@ public class VariableAndDataTypesDemo {
         boolean student = true;
 
         System.out.println("Student name : "+name);
-        System.out.println("Studentr Id : "+id);
+        System.out.println("Student Id : "+id);
         System.out.println("Height : "+height+"'" );
         System.out.println("Gender "+gender);
         System.out.println("Age > 18 :  "+student);
