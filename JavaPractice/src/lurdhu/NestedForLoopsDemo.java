6dd0dadf23c02f1ad3bbc986c7e2213e0ee7df77
PONNAPATI <lourdu9966@gmail.com>
2025-02-28 10:16:42 +0530

    for loop demo

---
+++ JavaPractice/src/lurdhu/NestedForLoopsDemo.java
@@ -0,0 +1,11 @@
+package src.lurdhu;
+
+public class NestedForLoopsDemo {
+    public static void main(String[] args) {
+        for(int i = 0; i < 5; i++){
+            for(int j = 1; j < 7; j++){
+                System.out.println("I value is :" + i + ", J value is:" + j);
+            }
+        }
+    }
+}
