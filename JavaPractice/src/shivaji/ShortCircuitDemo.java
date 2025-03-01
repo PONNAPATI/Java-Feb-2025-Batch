2992ef0aa4503383d4f6a8cf1915550bf5d4e543
shivajiparepally <pvshiva2101@gmail.com>
2025-03-01 00:34:10 -0500

    Added some Demo classes

--- JavaPractice/src/shivaji/ShortCircuitDemo.java
+++ JavaPractice/src/shivaji/ShortCircuitDemo.java
@@ -3,7 +3,7 @@ package shivaji;
 public class ShortCircuitDemo {
     public static void main(String[] args) {
         int i=12;
-        String  result = (i%2==0)? "Even" : "Odd";
+        String  result =(i%2==0)? "Even":"odd";
         System.out.println(result);
     }
 }
