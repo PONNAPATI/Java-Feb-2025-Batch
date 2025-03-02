67699d003537e95b81c502e9e8d6a4d10f7fd856
thummasreddy <sagarvijay6@gmail.com>
2025-03-02 11:27:19 -0500

    Committing for Arrays Single-Dimensional Array and Multi-Dimensional Arrays basic code examples.

---
+++ JavaPractice/src/sagar/Operators/ComparisonOperators.java
@@ -0,0 +1,17 @@
+package Operators;
+
+public class ComparisonOperators {
+    public static void main(String[] args) {
+        int v = 10;
+        int s = 7;
+
+        System.out.println("m == n: " + (v == s)); // false
+        System.out.println("m != n: " + (v != s)); // true
+        System.out.println("m > n: " + (v > s));   // true
+        System.out.println("m < n: " + (v < s));   // false
+        System.out.println("m >= n: " + (v >= s)); // true
+        System.out.println("m <= n: " + (v <= s)); // false
+    }
+}
+
+
