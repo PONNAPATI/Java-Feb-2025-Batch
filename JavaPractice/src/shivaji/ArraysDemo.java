71b607d8699f1fcdd665ae33926d2509b415bcb6
shivajiparepally <pvshiva2101@gmail.com>
2025-03-02 01:16:40 -0500

    Added some Demo classes

---
+++ JavaPractice/src/shivaji/ArraysDemo.java
@@ -0,0 +1,13 @@
+package shivaji;
+
+public class ArraysDemo {
+    public static void main(String[] args) {
+        String[] names = {"Swathi","Shiva","Yashu","Rajesh","Siddhu"};
+//        System.out.println(names[0]+ " - "+names[1]);
+        for (int i=0;i< names.length;i++)
+        {
+            System.out.println(names[i]);
+        }
+
+    }
+}
---
+++ JavaPractice/src/shivaji/MultiDimensionalArray.java
@@ -0,0 +1,15 @@
+package shivaji;
+
+public class MultiDimensionalArray {
+    public static void main(String[] args) {
+        int [][] matrix = {{10,20,30},{40,50,60},{70,80,90}};
+
+        for (int i=0;i<matrix.length;i++)
+        {
+            for (int j=0;j< matrix[i].length;j++)
+            {
+                System.out.println(matrix[i][j]);
+            }
+        }
+    }
+}
