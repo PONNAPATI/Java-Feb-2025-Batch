a3a9c3d0ad815e61b8a20fd19eb712690836bfa2
thummasreddy <sagarvijay6@gmail.com>
2025-03-05 20:41:06 -0500

    social media post with likes, comments, and user messages.

---
+++ JavaPractice/src/sagar/ClassAndMethods/socialmedia/Message.java
@@ -0,0 +1,22 @@
+package ClassAndMethods.socialmedia;
+
+public class Message {
+    /**
+     * This class represents a simple message between two users.
+     */
+    String sender;
+    String receiver;
+    String messageText;
+
+    // Constructor to create a message
+    public Message(String sender, String receiver, String messageText) {
+        this.sender = sender;
+        this.receiver = receiver;
+        this.messageText = messageText;
+    }
+    // Method to display the message details
+    public void showMessage(){
+        System.out.println("\nMessage from " + sender + " to " + receiver + ": " + messageText);
+    }
+
+}
