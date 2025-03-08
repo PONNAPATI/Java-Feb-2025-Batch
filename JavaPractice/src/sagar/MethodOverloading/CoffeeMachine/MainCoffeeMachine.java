59328e7db63b494c2f994e49a8ee0a56e1e9c0a0
thummasreddy <sagarvijay6@gmail.com>
2025-03-08 17:50:20 -0500

    Adding  Practice example on MethodOveLoading.

---
+++ JavaPractice/src/sagar/MethodOverloading/CoffeeMachine/MainCoffeeMachine.java
@@ -0,0 +1,11 @@
+package MethodOverloading.CoffeeMachine;
+
+public class MainCoffeeMachine {
+    public static void main(String[] args) {
+        CoffeeMachine machine = new CoffeeMachine();
+
+        machine.brewCoffee();
+        machine.brewCoffee("Large");
+        machine.brewCoffee("Medium", "Vanilla");
+    }
+}
