47c8bf434673ce5a393a281f455109fa2adcc8d0
dhinuvcn <dineshv17061998@gmail.com>
2025-03-13 04:48:22 -0400

    Inheritance of truck company

---
+++ JavaPractice/src/dinesh/OOPS/INHERITANCE/Truck_company.java
@@ -0,0 +1,9 @@
+package dinesh.OOPS.INHERITANCE;
+
+public class Truck_company {
+    String Company_name;
+
+    void Capacity(){
+        System.out.println("It is midlevel truck company");
+    }
+}
---
+++ JavaPractice/src/dinesh/OOPS/INHERITANCE/Truck_counts.java
@@ -0,0 +1,28 @@
+package dinesh.OOPS.INHERITANCE;
+
+public class Truck_counts extends Truck_company{
+
+    protected int Total_no_of_trucks(int Truck_count){
+
+        return Truck_count;
+    }
+
+    public int Active_trucks_on_road(){
+        int active_trucks = 129;
+        return active_trucks;
+    }
+
+    /*public int Active_trucks_on_road(int active_trucks){
+        return active_trucks;
+    }*/
+
+
+    public void Truck_counts(int ac_cabin_trucks){
+    }
+
+    boolean company_accident_records(boolean Is_anyone_died_due_to_accident){
+
+        return true;
+    }
+
+}
