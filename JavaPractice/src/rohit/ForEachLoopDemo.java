6944d03187f714459a073bf9c7279b60bfbdab55
rohithkrishnaborra <bvrohithkrishna@gmail.com>
2025-03-20 18:44:19 -0400

    Modified files

--- JavaPractice/src/rohit/ForEachLoopDemo.java
+++ JavaPractice/src/rohit/ForEachLoopDemo.java
@@ -1,6 +1,11 @@
 package rohit;
 
 public class ForEachLoopDemo {
+    @Override
+    public String toString() {
+        return "ForEachLoopDemo{}";
+    }
+
     public static void main(String[] args) {
         int[] test= {1,2,3,4};
         for (int var:test)
