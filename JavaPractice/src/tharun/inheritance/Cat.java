42bbeb0296461cd7def7a1c08dfea1c0676c8e95
Tharun-reddi <reddytharun657@gmail.com>
2025-03-21 21:09:27 +0530

    Inheritance Example problems

---
+++ JavaPractice/src/tharun/inheritance/Cat.java
@@ -0,0 +1,12 @@
+package tharun.inheritance;
+
+public class Cat extends Animal {
+    String color;
+
+    public void drinks(){
+        System.out.println("Milk");
+    }
+    public void walk(){
+        System.out.println("Dog is Walking");
+    }
+}
