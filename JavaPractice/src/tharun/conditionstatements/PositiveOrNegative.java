8f567ecb86660995efd26793047307be34e3fb46
Tharun-reddi <reddytharun657@gmail.com>
2025-03-23 13:57:43 +0530

    Conditional Statements practice problems

---
+++ JavaPractice/src/tharun/conditionstatements/PositiveOrNegative.java
@@ -0,0 +1,14 @@
+package tharun.conditionstatements;
+
+public class PositiveOrNegative {
+    public static void main(String[] args) {
+        int num = 4;
+
+        if(num > 0){
+            System.out.println("The number is Positive");
+        }else{
+            System.out.println("The number is Negative");
+        }
+    }
+
+}
